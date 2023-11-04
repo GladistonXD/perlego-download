3045e21f29229b49f6566b85f4d3bce205c1b863
GladistonXD <50533550+GladistonXD@users.noreply.github.com>
2023-11-04 05:02:40 -0300

    Update contentmanual.js

--- contentmanual.js
+++ contentmanual.js
@@ -1,30 +1,22 @@
-try{
+try {
 	const result = document.evaluate("/html/body/div[1]/main/div[1]/div[2]/div[3]/div/text()[3]", document, null, XPathResult.ANY_TYPE, null);
 	let text = '';
 	let node = result.iterateNext();
-
 	while (node) {
 		text += node.textContent;
 		node = result.iterateNext();
 	}
-
 	const pagefinal = text;
-
 	let stopButton = document.createElement('button');
 	stopButton.textContent = 'Quit and save';
 	stopButton.style.position = 'fixed';
 	stopButton.style.top = '54%';
 	stopButton.style.right = '47%';
 	stopButton.style.zIndex = '9999';
-
 	stopButton.style.backgroundColor = 'white';
 	stopButton.style.color = 'green'; 
 	stopButton.style.fontWeight = 'bold';
-
-
-
 	document.body.appendChild(stopButton);
-
 	let timeoutID;
 	let resultadosHTML = '<html><head><title>Resultados da Busca</title></head><body>';
 
@@ -43,17 +35,15 @@ try{
 
 	function procurarElemento(pagina) {
 		const elemento = document.evaluate(`//*[@id="p${pagina}--0"]/div/div[2]/object`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-		const elementox = document.evaluate(`//*[@id="p${pagina}--0"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+		const elementox = document.evaluate(`//*[@id="p${pagina}--0"]/div/div[2]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 		if (elemento) {
 			return elementox.innerHTML;
 		}
 		return null;
 	}
-
 	function rolarAteEncontrarPagina(pagina) {
 		if (pagina <= pagefinal) {
 			const conteudoElemento = procurarElemento(pagina);
-
 			if (conteudoElemento === null) {
 				const mensagem = document.createElement('div');
 				mensagem.textContent = `Page ${pagina-1}/${pagefinal} found. Keep scrolling to the page ${pagefinal}`;
@@ -67,10 +57,7 @@ try{
 				mensagem.style.zIndex = '9999';
 				mensagem.style.fontWeight = 'bold';
 				mensagem.id = 'mensagem'; 
-
 				document.body.appendChild(mensagem);
-
-
 				timeoutID = setTimeout(() => {
 					mensagem.remove();
 					rolarAteEncontrarPagina(pagina);
@@ -78,7 +65,6 @@ try{
 			} else {
 				console.log(`Conteúdo do elemento da página ${pagina} encontrado:`);
 				resultadosHTML += conteudoElemento;
-
 				if (pagina < pagefinal) {
 					rolarAteEncontrarPagina(pagina + 1);
 				} else {
@@ -88,9 +74,7 @@ try{
 			}
 		}
 	}
-
 	rolarAteEncontrarPagina(1);
-
 	function downloadResultados(resultados, nomeArquivo) {
 		const blob = new Blob([resultados], { type: 'text/html' });
 		const link = document.createElement('a');
@@ -98,7 +82,7 @@ try{
 		link.download = nomeArquivo;
 		link.click();
 	}
-}catch(error){
+} catch(error){
     if (error) {
         throw new Error();
     }
